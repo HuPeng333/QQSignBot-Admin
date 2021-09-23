254fdd7c66d0c8728290a09b78811e2b6f03c5f6
许德森 <2237803016@qq.com>
2021-09-23 17:49:50 +0800

    feature: 将Element-Plus改为了自动导入
    详细请见: https://element-plus.org/zh-CN/guide/quickstart.html#%E8%87%AA%E5%8A%A8%E5%AF%BC%E5%85%A5-%E6%8E%A8%E8%8D%90

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,28 @@
 // vue.config.js
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const components = require('unplugin-vue-components/webpack')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
+  // 添加全局样式
   chainWebpack: (config) => {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
     types.forEach((type) => addStyleResource(config.module.rule('scss').oneOf(type)))
+    config
+      .plugin('webpack-bundle-analyzer')
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+  },
+  configureWebpack: {
+    // 按需导入
+    plugins: [
+      components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
   }
 }
 
